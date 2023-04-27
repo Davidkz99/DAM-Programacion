6580fb1e53ddb4bf532adb51289e6c022694013f
David <David1234@gmail.com>
2023-04-27 10:27:40 +0200

    Proyecto 2742023

--- DataBaseConnection.java
+++ DataBaseConnection.java
@@ -4,7 +4,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-
 /**
  * Clase para habilitar la conexión a nuestra base de datos.
  * @author David
@@ -12,36 +11,17 @@ import java.sql.SQLException;
  */
 public class DataBaseConnection {
 	private Connection connection;
-	public boolean connect() {		
+	public boolean conectar() {		
 		try {
 			//cargar el driver
 			DriverManager.registerDriver (new com.mysql.cj.jdbc.Driver());			
-			
+
 			//crear un objeto de conexión
-			this.connection = 
-					DriverManager.getConnection("jdbc:mysql://localhost/anime?user=root&password=root");			
+			this.connection = DriverManager.getConnection("jdbc:mysql://localhost/anime?user=root&password=root");			
 		} catch (SQLException e) {			
 			e.printStackTrace();
 		}
 		return connection==null?false:true;
 	}
-	public boolean disconnect() {
-		try {
-			this.connection.close();
-			return true;
-		} catch (SQLException e) {			
-			return false;
-		}
-	}
-	public Connection getConnection() {
-		return this.connection;
-	}
-	public boolean isConnected() {
-		try {
-			return !this.connection.isClosed();
-		} catch (SQLException e) {			
-			e.printStackTrace();
-			return false;
-		}
-	}
+	
 }

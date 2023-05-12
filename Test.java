4d50ad647284f743ea85963c2c848e1276222329
David <David1234@gmail.com>
2023-05-12 10:25:06 +0200

    He probado a añadir una serie

--- Test.java
+++ Test.java
@@ -5,6 +5,12 @@ package dam.tema8.proyecto;
  */
 import java.util.ArrayList;
 
+import dam.tema8.proyecto.copia.Conexion;
+import dam.tema8.proyecto.copia.Estudio;
+import dam.tema8.proyecto.copia.Gestion;
+import dam.tema8.proyecto.copia.Pelicula;
+import dam.tema8.proyecto.copia.Serie;
+
 public class Test {
 	public static void main(String[] args) {
 		Conexion conexion = new Conexion();
@@ -19,13 +25,25 @@ public class Test {
 				System.out.println(serie);
 			}
 			System.out.println();
-			
+//			
 			//Mostrar todas las películas con todos sus atributos
 			ArrayList<Pelicula> peliculas = databaseManager.getPeliculas();
 			for (Pelicula pelicula : peliculas) {
 				System.out.println(pelicula);
 			}
+			System.out.println();
 			
+			//Añadir una serie a la base de datos. 
+			//EDITAR LA QUERY DE AÑADIR PARA QUE AÑADA CON LA ID QUE YO LE DÉ, NO LA DE AUTOINCREMENT
+			Estudio e1 = new Estudio(3);
+			Serie s1 = new Serie(4, "My Hero Academia", 12, e1);
+			boolean añadirSerie = databaseManager.crearSerie(s1);
+			if (añadirSerie == true) {
+				System.out.println("Se ha añadido la serie");
+			} else {
+				System.out.println("No se ha podido añadir la serie");
+			}
+			System.out.println();
 			//Mostrar una tabla filtrada por al menos por 2 campos
 //			ArrayList<Serie> seriesFiltradas = databaseManager.getSeries("nombre_serie", "Death Note");
 		}

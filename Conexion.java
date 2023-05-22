6babc374d6e4fc6cc1d3fda11ee5790339de1634
David <David1234@gmail.com>
2023-05-22 23:51:32 +0200

    He documentado la clase

--- Conexion.java
+++ Conexion.java
@@ -13,6 +13,10 @@ public class Conexion {
 	private Connection connection = null;
 	private String connectionString;
 	
+	/**
+	 * Constructor de la clase Conexion.
+	 * @param connectionString
+	 */
 	public Conexion(String connectionString) {		
 		//guarda los datos de conexión
 		this.connectionString = connectionString;
@@ -23,16 +27,23 @@ public class Conexion {
 			e.printStackTrace();
 		}
 	}
+	
 	/**
-	 * Guarda el objeto de conexión
-	 * @param connection
-	 * @return Si es distinto de null devuelve true
+	 * Método para establecer una conexión a la base de datos.
+	 * 
+	 * @param connection La conexión a establecer.
+	 * @return true si la conexión se estableció correctamente, false en caso contrario.
 	 */
 	public boolean conectar(Connection connection) {		
 		this.connection = connection;
 		return this.connection==null?false:true;
 	}
 
+	/**
+	 * Método para cerrar la conexión a la base de datos.
+	 * 
+	 * @return true si la conexión se cerró correctamente, false en caso contrario.
+	 */
 	public boolean disconnect() {
 		try {
 			if(this.connection==null) return true;
@@ -42,9 +53,20 @@ public class Conexion {
 			return false;
 		}
 	}
+	/**
+	 * Método getter para obtener la conexión actual.
+	 * 
+	 * @return La conexión actual.
+	 */
 	public Connection getConnection() {
 		return this.connection;
 	}
+
+	/**
+	 * Método para verificar si hay una conexión establecida.
+	 * 
+	 * @return true si hay una conexión establecida, false en caso contrario.
+	 */
 	public boolean estaConectado() {
 		try {
 			return !this.connection.isClosed();
@@ -53,6 +75,12 @@ public class Conexion {
 			return false;
 		}
 	}
+	
+	/**
+	 * Método para obtener la cadena de conexión a la base de datos.
+	 * 
+	 * @return La cadena de conexión a la base de datos.
+	 */
 	public String getConnectionString() {
 		return this.connectionString;
 	}
